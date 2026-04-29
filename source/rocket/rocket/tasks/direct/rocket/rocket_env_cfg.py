092bb126cea9f82eb70c7832906b61cb757f6d7a
Austin Tsai <aytsai@usc.edu>
2026-04-29 01:40:37 -0700

    u

--- source/rocket/rocket/tasks/direct/rocket/rocket_env_cfg.py
+++ source/rocket/rocket/tasks/direct/rocket/rocket_env_cfg.py
@@ -257,7 +257,7 @@ class RocketEnvCfg(DirectRLEnvCfg):
     # simulation
     sim: SimulationCfg = SimulationCfg(
         dt=1 / 200,
-        render_interval=1,
+        render_interval=decimation,
         physics_material=sim_utils.RigidBodyMaterialCfg(
             static_friction=1.0,
             dynamic_friction=1.0,
